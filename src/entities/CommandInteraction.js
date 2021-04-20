232940055146692969fad076bd9c19d2b99c010c
Navy <43297329+NavyCake@users.noreply.github.com>
2021-04-20 17:13:08 -0300

    Update CommandInteraction.js

--- src/entities/CommandInteraction.js
+++ src/entities/CommandInteraction.js
@@ -4,16 +4,22 @@ const CommandArgs = require('./CommandArgs')
 
 module.exports = class CommandInteraction {
     constructor(command) {
-        this.name = command.data.name
+        this.haveOptions = typeof command.options === 'undefined' ? false : true
+        this.name = command.name
         this.id = command.id
         this.options = new Array()
         this.noArgs = false
-        if (command.data.options >= 0) {
-            command.data.options.map(e => {
-                this.options.push(new CommandArgs(e))
-            })
+        if (this.haveOptions === true) {
+            if (command.options >= 0) {
+                command.options.map(e => {
+                    this.options.push(new CommandArgs(e))
+                })
+            } else {
+                this.noArgs = true
+            }
+          
         } else {
-            this.noArgs = true
+            this.noArgs = false
         }
     }
 }
