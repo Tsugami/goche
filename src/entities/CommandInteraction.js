3001856dd22d14a77e71dbd45f3626969912951b
Navy <43297329+NavyCake@users.noreply.github.com>
2021-04-20 13:01:43 -0300

    Github found a conflict I ended up deleting

---
+++ src/entities/CommandInteraction.js
@@ -0,0 +1,19 @@
+const CommandArgs = require('./CommandArgs')
+
+
+
+module.exports = class CommandInteraction {
+    constructor(command) {
+        this.name = command.data.name
+        this.id = command.id
+        this.options = new Array()
+        this.noArgs = false
+        if (command.data.options >= 0) {
+            command.data.options.map(e => {
+                this.options.push(new CommandArgs(e))
+            })
+        } else {
+            this.noArgs = true
+        }
+    }
+}
