010998900dda93e70f4d013ac2435f9a82026e01
Navy <43297329+NavyCake@users.noreply.github.com>
2021-05-08 15:38:23 -0300

    VoiceChannel extends Channel

--- src/entities/VoiceChannel.js
+++ src/entities/VoiceChannel.js
@@ -1,13 +1,23 @@
+const Channel = require('./Channel');
 const Guild = require('./Guild');
 const Member = require('./Member');
 
-module.exports = class VoiceChannel {
+module.exports = class VoiceChannel extends Channel {
 	constructor(channel, guild = new Guild()) {
-		this.id = channel.id || '';
+        /**
+         * @deprecated @version 0.0.2
+		```
+        this.id = channel.id || '';
 		this.name = channel.name;
 		this.type = 'voiceChannel';
 		this.guildID = guild.id;
 		this.guild = guild;
+        ```
+		
+        */
+		super(channel, guild);
+		
+
 		this.userLimit = channel.user_limit;
 		this.position = channel.position;
 		this.region =
